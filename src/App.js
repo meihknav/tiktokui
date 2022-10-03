e976196100db913f3408ec48cb0a5997bb8bd641
meihknav <vankhiemhtpc82002@gmail.com>
2022-10-03 14:46:27 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div style = {{padding: 20}}className="App">
+      <h1>Hello anh em </h1>
+      <h1>Tôi là Khiêm Hôm nay là ngày tôi bắt đầu làm dự án React</h1>
+      <h1>HAHAHAHAHAHH</h1>
+    </div>
+  );
+}
+
+export default App;
